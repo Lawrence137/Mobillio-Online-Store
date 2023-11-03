594dd4d66d1dea67619411485120574b5de44dc5
Lawrence Kimani <lawrencekimani18@gmail.com>
2023-11-03 11:39:15 +0300

    🔧 configured store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,26 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// import { persistReducer } from 'redux-persist';
-// import storage from 'redux-persist/lib/storage';
-// import cartReducer from './cartSlice';
+import { configureStore } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import cartReducer from './cartSlice';
 
-// const persistConfig = 
+// Configuration for redux-persist
+const persistConfig = {
+    key: 'root',
+    storage,
+}; 
+
+// Your root reducer, you can combine multiple reducers here
+const rootReducer = {
+    cart: cartReducer,
+    // other reducers go here
+};
+
+// Create a persisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Create the store with the persisted reducer
+const store = configureStore({
+    reducer: persistedReducer,
+});
+
+export default store;
